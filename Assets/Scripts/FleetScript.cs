72526893f9a1e867034852391d77c1238cfc4c74
Igor Józefowicz <igorjozefowicz@gmail.com>
2024-12-21 10:48:28 +0100

    develop FleetScript

--- Assets/Scripts/FleetScript.cs
+++ Assets/Scripts/FleetScript.cs
@@ -7,16 +7,47 @@ public class FleetScript : MonoBehaviour
 {
     public float moveTime = 2;
     public float moveSpeed = 0.3f;
+    int moveCounter = 0;
+    int moveDirection;
+    public bool isGameRunning;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveDirection = 1;
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        InvokeRepeating("Move", moveTime, moveTime);
+        isGameRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGameRunning && transform.childCount == 0)
+        {
+            StopGame(true);
+        }
+    }
+
+    public void StopGame(bool isWin)
+    {
+        CancelInvoke("Move");
+        isGameRunning = false;
+    }
+
+    void Move()
+    {
+        transform.position += new Vector3(moveSpeed * moveDirection, 0, 0);
+        moveCounter += moveDirection;
+
+        if (moveCounter <= -4 || moveCounter >= 4)
+        {
+            moveDirection *= -1;
+            transform.position -= new Vector3(0, moveSpeed, 0);
+        }
     }
 }
